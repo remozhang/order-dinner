62cab1d8de5289f826298b593ff3b128a6fe66ae
remozhang <814895077@qq.com>
2019-03-06 11:18:57 +0800

    重构了下代码结构, 添加了程序异常处理部分, 规定识别10次后终止

--- index.php
+++ index.php
@@ -1,47 +1,95 @@
 <?php
 require_once 'AipOcr.php';
 
+$config = require_once('src/config.php');
 // 你的 APPID AK SK
-const APP_ID = '15672391';
-const API_KEY = 'YiE5EkBPaA74ODNsarjQdMKl';
-const SECRET_KEY = 'q4eDMERT3ZdvtAZHZh6L6uITpB7DX7Eg';
-
-$client = new AipOcr(APP_ID, API_KEY, SECRET_KEY);
+$baiduAI = $config['baiduAI'];
+$client = new AipOcr($baiduAI['appId'], $baiduAI['apiKey'], $baiduAI['secretKey']);
 
 // 思路: 设置请求图片cookie与请求订餐cookie一致即可
-
 $fileUrl = 'http://techbbs.hi2000.com/dinner/test.php';
 $orderUrl = 'http://techbbs.hi2000.com/dinner/dinner.php';
 
-$savePath = './';
-$captcha = downloadCaptcha($fileUrl, $savePath,'zhanglei');
+$savePath = './img/';
+$phpSessId = rand();
+$captcha = downloadCaptcha($fileUrl, $savePath,$phpSessId);
+$orderAccount = $config['orderAccount'];
+order($client, $captcha, $orderAccount, $orderUrl, $phpSessId);
+function order($client, $captcha, $orderAccount, $orderUrl, $phpSessId, $i = 0)
+{
+    try {
+        $resultCaptcha = $client->basicAccurate($captcha);
 
-$captcha = $client->basicAccurate($captcha);
-$words = $captcha['words_result']['0']['words'];
+        if (!isset($resultCaptcha['words_result']['0']['words'])) {
+            throw new Exception('识别验证码识别失败!');
+        } else {
+            $words = $resultCaptcha['words_result']['0']['words'];
+        }
+        if (strlen($words) < 4 ) {
+            throw new Exception('验证码识别位数不够!');
+        }
 
-$data = array(
-    'username' => urldecode('%D5%C5%C0%D7'),
-    'validate' => '123456',
-    'reserve' => urldecode('%B6%A9%B2%CD'),
-    'code' => $words
-);
+        $data = array(
+            'username' => mb_convert_encoding($orderAccount['username'], 'GBK', 'UTF-8'),
+            'validate' => mb_convert_encoding($orderAccount['validate'], 'GBK', 'UTF-8'),
+            'reserve' => mb_convert_encoding('订餐', 'GBK', 'UTF-8'),
+            'code' => $words
+        );
 
-orderDinner($orderUrl, $data, 'zhanglei');
+        $response = orderDinner($orderUrl, $data, $phpSessId);
+        $response = mb_convert_encoding($response, 'UTF-8', 'GBK');
+        if (!preg_match('/订餐成功/', $response)) {
+            throw new Exception($response);
+        }
+        file_put_contents('error.log', date('Y-m-d H:i:s') . ': 订餐成功!' . "\n", FILE_APPEND);
 
+    } catch (Exception $e) {
+        $i++;
+        if ($i > 10) {
+            $log = '今天运行订餐已经连续失败10次!';
+            file_put_contents('error.log', date('Y-m-d H:i:s') . ': ' . $log . "\n", FILE_APPEND);
+
+        } else {
+            $log = $e->getMessage();
+            file_put_contents('error.log', date('Y-m-d H:i:s') . ': ' . $log . "\n", FILE_APPEND);
+            order($client, $captcha, $orderAccount, $orderUrl, $phpSessId, $i);
+        }
+    }
+
+}
+
+/**
+ * @param $url
+ * @param $data
+ * @param null $phpSessId
+ *
+ * @return mixed
+ */
 function orderDinner(
     $url,
     $data,
     $phpSessId = null
 ) {
+
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_POST, 0);
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_COOKIE, 'PHPSESSID=' . $phpSessId);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-    $haha = curl_exec($ch);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $response = curl_exec($ch);
     curl_close($ch);
+
+    return $response;
 }
 
+/**
+ * @param $fileUrl
+ * @param $savePath
+ * @param null $phpSessId
+ *
+ * @return bool|string
+ */
 function downloadCaptcha(
     $fileUrl,
     $savePath,
@@ -59,6 +107,3 @@ function downloadCaptcha(
 
     return file_get_contents($savePath . 'captcha.jpg');
 }
-
-
-
