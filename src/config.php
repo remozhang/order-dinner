14aa131959c010fc0eb2de05d24fcc829b14440d
remozhang <814895077@qq.com>
2019-03-07 14:49:59 +0800

    初始化配置文件

---
+++ src/config.php
@@ -0,0 +1,16 @@
+<?php
+
+$config = array();
+
+$config['orderAccount'] = array(
+    'username' => '张雷',
+    'validate' => '123456',
+);
+
+$config['baiduAI'] = array(
+    'appId' => '',
+    'apiKey' => '',
+    'secretKey' => ''
+);
+
+return $config;
