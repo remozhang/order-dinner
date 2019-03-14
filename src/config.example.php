f2d279f6a8cd4da5aee553becc668e9e3d3b3bb1
remozhang <814895077@qq.com>
2019-03-14 15:09:03 +0800

    添加了配置示例文件

---
+++ src/config.example.php
@@ -0,0 +1,16 @@
+<?php
+
+$config = array();
+
+$config['orderAccount'] = array(
+    'username' => '张雷',
+    'validate' => '88888',
+);
+
+$config['baiduAI'] = array(
+    'appId' => 'xxxx',
+    'apiKey' => 'xxxx',
+    'secretKey' => 'xxxx'
+);
+
+return $config;
